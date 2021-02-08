96963ee5296d29f0b870aa617cccd557098e1da5
Gevork <gevorg.chobanyan22@gmail.com>
2021-02-08 13:27:21 +0300

    installed node, added Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\carParkingCustomerController;
 
-//READ
-Route::get('/404', [carParkingCustomerController::class, 'errorHandler1']);
-Route::get('/500', [carParkingCustomerController::class, 'errorHandler2']);
-Route::get('/', [carParkingCustomerController::class, 'index']);
-Route::get('/clients', [carParkingCustomerController::class, 'index']);
-Route::get('clients/create_form', [carParkingCustomerController::class, 'createClientForm']);
-Route::get('/clients/edit/{client_id}', [carParkingCustomerController::class, 'editClientForm']);
-Route::get('/vehicle/parked', [carParkingCustomerController::class, 'parkedVehicles']);
-//CREATE
-Route::post('/clients/add', [carParkingCustomerController::class, 'createClient']);
-Route::post('/vehicle/add', [carParkingCustomerController::class, 'addVehicle']);
-//UPDATE
-Route::put('/vehicle/update/{vehicle_id}', [carParkingCustomerController::class, 'updateVehicle']);
-Route::put('/vehicle/update/parked/{vehicle_id}', [carParkingCustomerController::class, 'vehicleParkedStatusParked']);
-Route::put('/vehicle/update/gone/{vehicle_id}', [carParkingCustomerController::class, 'vehicleParkedStatusGone']);
-Route::put('/clients/update/{client_id}', [carParkingCustomerController::class, 'updateClient']);
-//DELETE
-Route::delete('/vehicle/{vehicle_id}', [carParkingCustomerController::class, 'deleteVehicle']);
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
