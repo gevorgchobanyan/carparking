09b4246e759829ea4b5306e3e795e4d47ccbab7a
Gevork <gevorg.chobanyan22@gmail.com>
2021-02-09 11:59:16 +0300

    ED-58 changed background color of the app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,13 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+{{--        <style> body {background-color: dimgrey} </style>--}}
+
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased ">
 
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,12 +12,15 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+{{--        <style> body {background-color: dimgrey} </style>--}}
+
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
-        <div class="font-sans text-gray-900 antialiased">
+        <div class="font-sans text-gray-900 antialiased ">
+
             {{ $slot }}
         </div>
     </body>
